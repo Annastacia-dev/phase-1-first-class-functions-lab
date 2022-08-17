7b7e70349dffc533134f3c59d7715a3aad55cf5f
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-08-17 07:50:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (integer1) {
+    return function fareMultiplier(integer2){
+       return integer1 * integer2;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, driversArray){
+    return driversArray(drivers);
+}
